0e503829d8428725beeebd43b94f10f75aa91651
mgcg216 <mgcg216@gmail.com>
2018-02-28 19:29:53 -0800

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.listen(process.env.PORT || 3000);
 
 // PathLocationStrategy
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
-})
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 console.log('Console listening!');
