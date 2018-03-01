9a174ed312791b6fdc4112bd03a9af83661e53ed
mgcg216 <mgcg216@gmail.com>
2018-02-28 23:24:42 -0800

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ app.use(express.static(__dirname + '/dist'));
 //app.use(express.static('public'));
 //app.use(express.static(__dirname + '/src'));
 app.set(process.env.PORT || 3000);
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
+//Start Server
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 // PathLocationStrategy
 
