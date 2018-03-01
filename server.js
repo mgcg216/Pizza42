6d476587493f22df92e4d42d864f0b9a801b6ad9
mgcg216 <mgcg216@gmail.com>
2018-02-28 16:44:27 -0800

    Initial commit

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/src'));
 app.listen(process.env.PORT || 3000);
 
 // PathLocationStrategy
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
 console.log('Console listening!');
